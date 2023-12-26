46e87f65f88054971cd08e2402a916579f3dd043
dasilvar <remi.dasilva@edu.esiee.fr>
2023-12-26 20:18:33 +0800

    feat: add first form with inits

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
+from models import db
+from forms import LoginForm, UpdateForm
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YourSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/french_resort'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = LoginForm()
+
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/models/__init__.py
@@ -0,0 +1,5 @@
+from models.alchemy import db
+from models.Booking import Booking
+from models.Guest import Guest
+from models.Room import Room
+from models.User import User
