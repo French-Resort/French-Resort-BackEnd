8a090a891abcf3a1ea8205a364294d39b373711a
HugoHLZhang <hugo.zhang@edu.esiee.Fr>
2024-01-02 11:12:29 +0800

    Update BookingsResource.py

--- src/resources/BookingsResource.py
+++ src/resources/BookingsResource.py
@@ -6,6 +6,6 @@ from models import Booking
 class BookingsResource(Resource):
     def get(self):
         bookings: list[Booking] = BookingService.get_all_bookings()
-        booking_list = [{'id_booking': booking.id_booking, 'check_in_date': booking.check_in_date.strftime('%Y-%m-%d'), 'check_out_date': booking.check_out_date.strftime('%Y-%m-%d'), 'total_price': booking.total_price, 'id_guest': booking.id_guest, 'id_guest': booking.id_room}
+        booking_list = [{'id_booking': booking.id_booking, 'check_in_date': booking.check_in_date.strftime('%Y-%m-%d'), 'check_out_date': booking.check_out_date.strftime('%Y-%m-%d'), 'total_price': booking.total_price, 'id_guest': booking.id_guest, 'id_room': booking.id_room}
                         for booking in bookings]
         return jsonify({'bookings': booking_list, 'size': len(booking_list)})
